2f8a4d4fce06303fe5babb94d3e2f2ebdb4142f9
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-09 16:45:29 +0500

    Learn Constructor Function

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // JS is all about Objects
 
 console.log(
+  '-------------------------- Constructor Property ----------------------------------------'
+);
+
+console.log(
+  '-------------------------- Constructor Function ----------------------------------------'
+);
+
+// Constructor Function
+function Circle(radius) {
+  console.log('this', this);
+  this.radius = radius;
+  this.draw = () => {
+    console.log('draw');
+  };
+}
+
+const circle1 = new Circle(1);
+circle1.draw();
+
+console.log(
   '-------------------------- Factory Function ----------------------------------------'
 );
 
